79f76bfedaf70615dd98ce9aaa986259424a42da
Justin Tadlock <justintadlock@gmail.com>
2025-07-09 18:07:40 -0500

    Proof of concept
    Adds initial files and folders.

---
+++ public/css/social-links.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ede16dbb0c8b32b5ac5e');
---
+++ public/js/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks', 'wp-primitives'), 'version' => '82ed176cfc31723a1558');
